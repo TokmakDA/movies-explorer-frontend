24c0ac3e21e70d2f6ed8ffbf32a4e5f0e05baee4
Dmitry <113120694+TokmakDA@users.noreply.github.com>
2023-06-10 19:07:51 +0300

    adaptiv AboutProject

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -6,23 +6,27 @@ function AboutProject() {
     <section className="content__section about-project">
       <h2 className="content__title">О проекте</h2>
       <hr className="content__title-line" />
-      <div className="about-project__description-container">
-        <h3 className="about-project__subtitle">
-          Дипломный проект включал 5 этапов
-        </h3>
-        <p className="about-project__description">
-          Составление плана, работу над бэкендом, вёрстку, добавление
-          функциональности и финальные доработки.
-        </p>
-        <h3 className="about-project__subtitle">
-          На выполнение диплома ушло 5 недель
-        </h3>
-        <p className="about-project__description">
-          У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
-          соблюдать, чтобы успешно защититься.
-        </p>
+      <div className="about-project__description-wrapper">
+        <div className="about-project__description-container">
+          <h3 className="about-project__subtitle">
+            Дипломный проект включал 5 этапов
+          </h3>
+          <p className="about-project__description">
+            Составление плана, работу над бэкендом, вёрстку, добавление
+            функциональности и финальные доработки.
+          </p>
+        </div>
+        <div className="about-project__description-container">
+          <h3 className="about-project__subtitle">
+            На выполнение диплома ушло 5 недель
+          </h3>
+          <p className="about-project__description">
+            У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
+            соблюдать, чтобы успешно защититься.
+          </p>
+        </div>
       </div>
-      <div className="about-project__timeline-container">
+      <div className="about-project__timeline-wrapper">
         <div className="about-project__timeline about-project__timeline_color_green">
           1 неделя
         </div>
